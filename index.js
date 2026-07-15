107e4139eafbb399e08bb1763264aaee80cf29e2
jahangirjs450-sudo <jahangirjs450@gmail.com>
2026-07-15 18:26:15 +0600

    mini project

---
+++ index.js
@@ -0,0 +1,17 @@
+// javaScript start
+let fullName = " Name: Jahangir";
+let age = " Age: 22";
+let country = " Country: Bangladesh";
+let favoriteLanguage = " Favorite Language: JavaScript";
+let dreamJob = "Dream Job: Full-Stake Developer";
+let yearsToGoal ="Years to Reach Goal : 1" ;
+let isLearningJs ="Learning JavaScript : true";
+
+console.log(fullName)
+console.log(age)
+console.log(country)
+console.log(favoriteLanguage)
+console.log(dreamJob)
+console.log(yearsToGoal)
+console.log(isLearningJs)
+
