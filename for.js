6a4a264668bbaeeac8bcb71a7f34a5d87de9c9ab
jahangirjs450-sudo <jahangirjs450@gmail.com>
2026-07-18 16:50:28 +0600

    for loop practise

---
+++ for.js
@@ -0,0 +1,60 @@
+// let count = 5;
+// count<45;
+// count++;
+// for(let count=5; count<45; count++){
+//     console.log('Hero & Heroien')
+// }
+
+// let money = 5000;
+// money>1000;
+// money=money-10;
+// for(let money = 5000;money>1000; money= money-10){
+//     console.log('return of value')
+// }
+// let product =100;
+
+// while("product<400"){console.log(product);
+//     product=product+ 50;
+
+// }
+// let count =5;
+// while(count<50){
+//     console.log(count);
+//     count+=5;
+// }
+
+// let i= 899;
+// for(let i=899;i<910;i++){
+//     console.log(
+//         i
+//     )
+// }
+// const x=2;
+// number= 10;
+// for(let x=1; x<10;x++){
+//     console.log(x, "X",number, "=", x*number)
+// }
+// const number=2;
+
+
+// for(let x=1; x<=10;x++){
+//      console.log(x, "X",number, "=", x*number)
+//  }
+// const number=3;
+
+
+// for(let x=1; x<=10;x++){
+//      console.log(x, "X",number, "=", x*number)
+//  }
+// const number=4;
+
+
+// for(let x=1; x<=10;x++){
+//      console.log(x, "X",number, "=", x*number)
+//  }
+const number=7;
+
+
+for(let x=1; x<=10;x++){
+     console.log(x, "X",number, "=", x*number)
+ }
